2e34b8e1151fb630a7a30f02fd8b36b07d80ee1f
abdel <abdy.003@gmail.com>
2020-11-03 23:25:04 +0100

    test

--- index.php
+++ index.php
@@ -1,1 +1,41 @@
-<?php
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Login V16</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+<div class="limiter">
+    <div class="container" style="background-image: url('images/bg-01.jpg');">
+        <div class="formulaire">
+
+				<span class="title">
+					Authentification
+				</span>
+
+            <form class="form" action="login.php" method="post">
+                <div class="input-class">
+                    <input class="input-text" type="text" name="login" placeholder="Votre login">
+                </div>
+
+                <div class="input-class">
+                    <input class="input-text" type="password" name="mdp" placeholder="Votre mot de passe">
+                </div>
+
+              <div class="button-login">
+                  <button class="form-btn" type="submit">
+                      Login
+                  </button>
+              </div>
+
+            </form>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
